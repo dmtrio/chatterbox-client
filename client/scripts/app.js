108e6fbed55d9b28b5d718e1d34db64ef893ff92
Hack Reactor Student <student@hackreactor.com>
2017-07-07 14:07:44 -0500

    Add author names to bower.json, add app id and key to config.json

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,7 @@
-// YOUR CODE HERE:
+// http://parse.atx.hackreactor.com/chatterbox/classes/messages
+// display messages
+// refresh messages (automatic or triggered) 
+// some way for users to set their username and to send messages using that name
+// filter messages based on .roomname
+// should be able to add friends by clicking on their names
+  // those friends' messages should show up in bold
